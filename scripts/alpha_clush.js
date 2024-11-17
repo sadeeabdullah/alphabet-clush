cd9fe78acb0ab8afb2faa6de41007d56d8c23104
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-11-17 09:22:16 +0600

    resolved the issue of not getting element while changing  the bg color

--- scripts/alpha_clush.js
+++ scripts/alpha_clush.js
@@ -1,27 +1,23 @@
-function conitinueGame(params) {
-    //setting the random alpahabet as the text on 
-    document.getElementById('random-alpha').innerText=randomAlphabets();
-    const alpahabet=randomAlphabets()
-    console.log(alpahabet)
+                                    
 
-    // add background to the random alphabet on keyboard
-    addBg(alpahabet.toLowerCase())
-}
-function play(){              
-    // const homeSection = document.getElementById('home-section')
-    // const displaySection = document.getElementById('display-section')
-    // homeSection.classList.add('hidden')
-    // displaySection.classList.remove('hidden')
-    hideElement('home-section')
-    showElement('display-section')
-    
-    
-     //setting the random alpahabet as the text on 
-     const randomAlpha = randomAlphabets();
-     const newText =document.getElementById('random-alpha').innerText= randomAlpha
-     
-    
 
-     // add background to the random alphabet on keyboard
-     addBg(randomAlpha.toLowerCase())
-}                                     
+    function continueGame() {
+        //setting the random alpahabet as the text on 
+        const randomAlpha = randomAlphabets();
+
+        const currentAlphabet = document.getElementById('random-alpha');
+        currentAlphabet.innerText= randomAlpha;
+        
+       
+        
+        // add background to the random alphabet on keyboard
+        setBackgroundColorById(randomAlpha)
+    }
+    function play(){     
+
+        hideElement('home-section')
+        showElement('display-section')
+        continueGame()
+        
+    } 
+  
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,14 +8,23 @@ function showElement(elementId){
 }
 
 
-function randomAlphabets() {
-    const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    const randomIndex = Math.floor(Math.random()*alphabets.length)
-    const randomAlp = alphabets[randomIndex]
-    return randomAlp;
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
 }
 
-function addBg(elementId){
-    console.log(elementId)
-    document.getElementById(elementId).classList.add('bg-orange-400')
+
+function randomAlphabets() {
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
 }
+
+
+
+
